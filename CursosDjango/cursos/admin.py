96220e8a5f72ae8137d93f729684e60b4d097e88
Irwin <irwin1409@gmil.com>
2022-08-07 21:50:23 +0200

    okokok

--- CursosDjango/cursos/admin.py
+++ CursosDjango/cursos/admin.py
@@ -11,6 +11,9 @@ class AdministrarModeloCursos(admin.ModelAdmin):
     search_fields = ('idcurso','nombre','activo','duracion')
     date_hierarchy = 'created'
     list_filter = ('nombre','activo')
+    ist_per_page=2
+    list_display_links=('nombre','activo')
+    list_editable=('duracion',)
 
 admin.site.register(Cursos,AdministrarModeloCursos)
 
